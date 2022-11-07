969f06f78c1d5051004951c4863677c92dbdd764
froaky <jeet20208@iiitd.ac.in>
2022-11-08 01:28:03 +0530

    feat: Add functionality to the ALU module

--- simulator/components/ALU.py
+++ simulator/components/ALU.py
@@ -1,12 +1,23 @@
-MAX_VAL = 2 ** 32 - 1
+CEIL = (2 ** 31)
+MAX = (2 ** 31) - 1
+MIN = -(2 ** 32)
+
+def resolveOverflow(val):
+    '''If the computed value is out of range, return the mod CEIL value'''
+    if (val > MAX or val < MIN):
+        return val % CEIL
 
 def ADD(in_1, in_2):
     '''Returns the sum of input values'''
-    return (in_1 + in_2) % MAX_VAL
+    sum = in_1 + in_2
+    sum = resolveOverflow(sum)
+    return sum
 
 def SUB(in_1, in_2):
     '''Returns the difference of input values'''
-    return (in_2 - in_1) % MAX_VAL
+    diff = in_2 - in_1
+    diff = resolveOverflow(diff)
+    return diff
 
 def OR(in_1, in_2):
     '''Returns the bitwise OR of input values'''
@@ -17,12 +28,32 @@ def AND(in_1, in_2):
     return in_1 & in_2
 
 def SLL(in_1, in_2):
-    '''Returns the result of applying logical left shift to in_1, in_2 number of times'''
-    pass
+    '''
+    Returns the result of applying logical left shift to in_1, in_2 % 32 number of times
+    in_2 % 32 corresponds to the value given by least 5 bits of in_2
+    '''
+    shift = in_2 % 32
+    res = in_1 << shift
+    res = resolveOverflow(res)
+    return res
 
 def SRA(in_1, in_2):
-    '''Returns the result of applying logical left shift to in_1, in_2 number of times'''
-    pass
+    '''
+    Returns the result of applying arithmetic right shift to in_1, in_2 % 32 number of times
+    in_2 % 32 corresponds to the value given by least 5 bits of in_2
+    '''
+    shift = in_2 % 32
+    return in_1 >> shift
 
-def getResult(rs1_data, rs2_data, funct):
-    alu_dict = {''}
+def getResult(in_1, in_2, funct):
+    '''Maps funct bits to the function of the operation they represent'''
+    alu_dict = {
+        '0000000000' : ADD,
+        '0100000000' : SUB,
+        '0000000001' : SLL,
+        '0100000101' : SRA,
+        '0000000110' : OR,
+        '0000000111' : AND
+        }
+    
+    return alu_dict[funct](in_1, in_2)
